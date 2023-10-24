277663560d6d65cd214bebd3c9f892acaa6e747b
Marcele <marcelecardozo@gmail.com>
2023-10-23 23:02:52 -0300

    installing cors

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { userRoutes } from "./routes/user.routes";
 import { tweetRoutes } from "./routes/tweet.routes";
 import { authRoutes } from "./routes/auth.routes";
 import { likeRoutes } from "./routes/like.routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", userRoutes());
 app.use("/auth", authRoutes());
